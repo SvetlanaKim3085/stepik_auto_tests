4b3fee4543e07028461250844155d58b7bbd66e0
SvetlanaKim3085 <svetlana.kim8530@gmail.com>
2019-11-28 21:02:13 +1100

    main_page.py changed

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        link = self.browser.find_element_by_css_selector("#login_link")
+        link.click() 
