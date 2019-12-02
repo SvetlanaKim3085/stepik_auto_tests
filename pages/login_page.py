0002803bb00e617b4f8158a4b791b0e862a1214e
SvetlanaKim3085 <svetlana.kim8530@gmail.com>
2019-12-02 12:03:08 +1100

    registration form and login form added

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Login link is not found
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*MainPageLocators.Login_Form), "Login form  is not presented"    
+       
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*MainPageLocators.Registration_Form), "Registration form  is not presented"
+    
+        
