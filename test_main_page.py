d661b95dd709c283d20e2b3c4bc024f98aa9d6ff
SvetlanaKim3085 <svetlana.kim8530@gmail.com>
2019-12-06 09:56:49 +1100

    method ie deleted

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,24 @@
 
 from .pages.basket_page import BasketPage
+import pytest
 
 
+@pytest.mark.login
+class TestLoginFromProductPage():
+  
 
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self,browser):
+        link=link = "http://selenium1py.pythonanywhere.com"
+        page = BasketPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        page.basket_empty()
+        page.empty_basket_line()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link=link = "http://selenium1py.pythonanywhere.com"
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.basket_empty()
-    page.empty_basket_line()
-
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page=BasketPage(browser,link)
-    page.open()
-    page.go_to_basket()
-    page.basket_empty()
-    page.empty_basket_line()
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page=BasketPage(browser,link)
+        page.open()
+        page.go_to_basket()
+        page.basket_empty()
+        page.empty_basket_line()
